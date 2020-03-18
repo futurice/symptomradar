e3156a18a42edcc99c60669cdbe3f57e038e7d2f
Jarno Rantanen <jarno.rantanen@futurice.com>
2020-03-18 14:35:35 +0200

    Update backend build script to not care about pwd.

--- backend/build.sh
+++ backend/build.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# Exit if any of the following commands exit with non-0, and echo our commands back
-set -ex
-
-# For running npm "binaries"
-PATH=$PATH:./node_modules/.bin
+set -euo pipefail # exit on error; treat unset variables as errors; exit on errors in piped commands
+SELF_DIR="$(perl -e 'use File::Basename; use Cwd "abs_path"; print dirname(abs_path(@ARGV[0]));' -- "$0")" # figure out the absolute path to the current script, regardless of pwd (perl is more cross-platform than realpath; https://stackoverflow.com/a/30795461)
+PATH=$PATH:$SELF_DIR/node_modules/.bin # for running npm "binaries"
+cd $SELF_DIR # ensure correct pwd
 
 # Check that we're running the correct version of node
 check-node-version --package

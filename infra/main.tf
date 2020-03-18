cc8e21edd7dc1aa575b74bce3584c58cd6d3bb68
Jarno Rantanen <jarno.rantanen@futurice.com>
2020-03-18 16:58:58 +0200

    Add invocation of the main module for the dev-env.

---
+++ infra/main.tf
@@ -0,0 +1,17 @@
+# This is the DNS Zone to which we assume we can create DNS records
+resource "aws_route53_zone" "vigilant_sniffle_com" {
+  name = "vigilant-sniffle.com"
+}
+
+# Implements an instance of the app, for a specific env
+module "env_dev" {
+  source = "./modules/main"
+  providers = { aws.us_east_1 = aws.us_east_1 } # this alias is needed because ACM is only available in the "us-east-1" region
+
+  name_prefix = "vigilant-sniffle-dev-"
+}
+
+# Pass along any output from the instantiated module
+output "dev" {
+  value = module.env_dev
+}
---
+++ infra/providers.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  version = "~> 2.53"
+  profile = "vigilant-sniffle"
+  region  = "eu-central-1"
+}
+
+# This alias is needed because ACM is only available in the "us-east-1" region
+provider "aws" {
+  alias   = "us_east_1"
+  version = "~> 2.53"
+  profile = "vigilant-sniffle"
+  region  = "us-east-1"
+}
+
+provider "random" {
+  version = "~> 2.2"
+}
+
+provider "archive" {
+  version = "~> 1.3"
+}
+
+provider "template" {
+  version = "~> 2.1"
+}

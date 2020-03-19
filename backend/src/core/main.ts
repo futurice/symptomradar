2918b465f422bf8617e3ac76d274052e532cc1af
Jarno Rantanen <jarno.rantanen@futurice.com>
2020-03-19 13:48:44 +0200

    Refactor storage path to be easier to partition.

--- backend/src/core/main.ts
+++ backend/src/core/main.ts
@@ -5,10 +5,11 @@ const bucket: string = 'vigilant-sniffle-dev-storage';
 
 export function storeResponseInS3(uuid: string, firstName: string, favoriteColor: string) {
   const timestamp = new Date().toISOString();
+  const [date, time] = timestamp.split('T');
   return s3
     .putObject({
       Bucket: bucket,
-      Key: `responses/${uuid}/${timestamp}.json`,
+      Key: `responses/${date}/${time}/${uuid}.json`,
       Body: JSON.stringify({ uuid, timestamp, firstName, favoriteColor }),
       ACL: 'private',
     })

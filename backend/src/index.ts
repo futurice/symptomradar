46a7fe1e86d17857a286a6497881e0bef82afe24
Jarno Rantanen <jarno.rantanen@futurice.com>
2020-03-19 10:51:34 +0200

    Add CLI entrypoint to backend, for testing locally.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,17 @@
-import { APIGatewayProxyHandler, Handler } from "aws-lambda";
-import { KEY } from "./common/const";
+import { APIGatewayProxyHandler, Handler } from 'aws-lambda';
+import { KEY } from './common/const';
 
 export const apiEntrypoint: APIGatewayProxyHandler = () => {
   return Promise.resolve({
     statusCode: 200,
-    body: JSON.stringify({ [KEY]: "api" }, null, 2)
+    body: JSON.stringify({ [KEY]: 'api' }, null, 2),
   });
 };
 
 export const workerEntrypoint: Handler<unknown> = () => {
-  console.log({ [KEY]: "worker" });
+  console.log({ [KEY]: 'worker' });
 };
+
+if (process.argv[0].match(/\/ts-node$/)) {
+  console.log(`${KEY}: CLI`);
+}

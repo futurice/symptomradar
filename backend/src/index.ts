0d8a7235061af09830d543c10f04d39e236b44d7
Jarno Rantanen <jarno.rantanen@futurice.com>
2020-03-19 09:23:56 +0200

    Add first code shared between frontend/api/worker.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,13 @@
 import { APIGatewayProxyHandler, Handler } from "aws-lambda";
+import { KEY } from "./common/const";
 
 export const apiEntrypoint: APIGatewayProxyHandler = () => {
   return Promise.resolve({
     statusCode: 200,
-    body: JSON.stringify({ hello: "api" }, null, 2)
+    body: JSON.stringify({ [KEY]: "api" }, null, 2)
   });
 };
 
 export const workerEntrypoint: Handler<unknown> = () => {
-  console.log({ hello: "worker" });
+  console.log({ [KEY]: "worker" });
 };

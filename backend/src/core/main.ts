076f747ad1a73dfbc77c3406345ccf4605253cf6
Jarno Rantanen <jarno.rantanen@futurice.com>
2020-03-19 17:18:16 +0200

    Add basic validation functionality to backend.

--- backend/src/core/main.ts
+++ backend/src/core/main.ts
@@ -1,16 +1,18 @@
 import * as AWS from 'aws-sdk';
+import { ResponseModelT } from '../common/model';
 
 const s3: AWS.S3 = new AWS.S3({ apiVersion: '2006-03-01' });
 const bucket: string = 'vigilant-sniffle-dev-storage';
 
-export function storeResponseInS3(uuid: string, firstName: string, favoriteColor: string) {
+export function storeResponseInS3(response: ResponseModelT) {
   const timestamp = new Date().toISOString();
   const [date, time] = timestamp.split('T');
+  const r = { ...response, timestamp }; // browser clock may be wrong/fraudulent, so overwrite with server time
   return s3
     .putObject({
       Bucket: bucket,
-      Key: `responses/${date}/${time}/${uuid}.json`,
-      Body: JSON.stringify({ uuid, timestamp, firstName, favoriteColor }),
+      Key: `responses/raw/${date}/${time}/${r.participantUuid}.json`,
+      Body: JSON.stringify(r),
       ACL: 'private',
     })
     .promise()
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,20 @@
 import { APIGatewayProxyHandler, Handler } from 'aws-lambda';
 import { KEY } from './common/const';
+import { assertIs, ResponseModel } from './common/model';
 import { storeResponseInS3 } from './core/main';
-import { v4 as uuidV4 } from 'uuid';
 
-export const apiEntrypoint: APIGatewayProxyHandler = () => {
-  return Promise.resolve({
-    statusCode: 200,
-    body: JSON.stringify({ [KEY]: 'api' }, null, 2),
+export const apiEntrypoint: APIGatewayProxyHandler = (event, context) => {
+  console.log(`Incoming request: ${event.httpMethod} ${event.path}`, {
+    headers: event.headers,
+    queryString: event.queryStringParameters,
+    body: event.body,
   });
+  return Promise.resolve()
+    .then(() => JSON.parse(event.body || '') as unknown)
+    .then(assertIs(ResponseModel))
+    .then(storeResponseInS3)
+    .then(() => response(200, { success: true }))
+    .catch(err => response(500, { error: true }, err));
 };
 
 export const workerEntrypoint: Handler<unknown> = () => {
@@ -16,10 +23,17 @@ export const workerEntrypoint: Handler<unknown> = () => {
 
 if (process.argv[0].match(/\/ts-node$/)) {
   console.log(`${KEY}: CLI`);
-  const colors = ['red', 'green', 'blue', 'yellow', 'orange', 'cyan', 'purple'];
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  storeResponseInS3(uuidV4(), 'Bob', color).then(
-    res => console.log('SUCCESS', res),
-    err => console.log('ERROR', err),
-  );
 }
+
+const response = (statusCode: number, body: object, logError?: Error) => {
+  console.log(`Outgoing response: ${statusCode}`, { body });
+  if (logError) console.error('ERROR', logError);
+  return {
+    statusCode,
+    body: JSON.stringify(body, null, 2),
+    headers: {
+      'Content-Type': 'application/json',
+      'Cache-Control': 'no-store,must-revalidate',
+    },
+  };
+};

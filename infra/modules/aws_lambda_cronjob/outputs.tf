0e943069f11078fedced1dfc537ed19ad560b7f9
Jarno Rantanen <jarno.rantanen@futurice.com>
2020-03-20 12:21:23 +0200

    Output the role of the function that was created by the Lambda modules.

--- infra/modules/aws_lambda_cronjob/outputs.tf
+++ infra/modules/aws_lambda_cronjob/outputs.tf
@@ -3,3 +3,7 @@ output "function_name" {
   value       = local.function_id
 }
 
+output "function_role" {
+  value       = aws_iam_role.this.name
+  description = "The IAM role for the function created; can be used to attach additional policies/permissions"
+}

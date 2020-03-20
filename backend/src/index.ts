5052f1c44cd2f57f57d661358b130291f13d097b
Jarno Rantanen <jarno.rantanen@futurice.com>
2020-03-20 13:23:51 +0200

    Update CLI entrypoint to function as sample data populator.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,5 @@
 import { APIGatewayProxyHandler, Handler } from 'aws-lambda';
+import { v4 as uuidV4 } from 'uuid';
 import { KEY } from './common/const';
 import { assertIs, ResponseModel } from './common/model';
 import { storeResponseInS3 } from './core/main';
@@ -22,7 +23,19 @@ export const workerEntrypoint: Handler<unknown> = () => {
 };
 
 if (process.argv[0].match(/\/ts-node$/)) {
+  const colors = ['red', 'green', 'blue', 'yellow', 'orange', 'cyan', 'purple'];
   console.log(`${KEY}: CLI`);
+  Promise.resolve()
+    .then(() => ({
+      participantUuid: uuidV4(),
+      responseTimestamp: new Date().toISOString(),
+      firstName: 'Bob',
+      favoriteColor: colors[Math.floor(Math.random() * colors.length)],
+    }))
+    .then(assertIs(ResponseModel))
+    .then(() => response(200, { success: true }))
+    .catch(err => response(500, { error: true }, err))
+    .then(res => console.log(res));
 }
 
 const response = (statusCode: number, body: object, logError?: Error) => {

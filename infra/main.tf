b02a3e43222caba6e2cbb180bfdd61cfc9eb5383
Jarno Rantanen <jarno.rantanen@futurice.com>
2020-03-20 17:28:49 +0200

    Refactor root module with consistent naming & tagging.

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,7 @@
 # This is the DNS Zone to which we assume we can create DNS records
 resource "aws_route53_zone" "vigilant_sniffle_com" {
   name = "vigilant-sniffle.com"
+  tags = var.tags
 }
 
 # Implements an instance of the app, for a specific env
@@ -8,11 +9,12 @@ module "env_dev" {
   source    = "./modules/main"
   providers = { aws.us_east_1 = aws.us_east_1 } # this alias is needed because ACM is only available in the "us-east-1" region
 
-  name_prefix       = "vigilant-sniffle-dev-"
+  name_prefix       = "${var.name_prefix}-dev"
+  tags              = merge(var.tags, { Environment = "dev" })
   frontend_password = var.frontend_password
 }
 
 # Pass along any output from the instantiated module
-output "dev" {
+output "env_dev" {
   value = module.env_dev
 }
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,3 +1,18 @@
+variable "name_prefix" {
+  description = "Name prefix to use for objects that need to be created (only lowercase alphanumeric characters and hyphens allowed, for S3 bucket name compatibility)"
+  default     = "vigilant-sniffle"
+}
+
+variable "tags" {
+  description = "AWS Tags to add to all resources created (where possible); see https://aws.amazon.com/answers/account-management/aws-tagging-strategies/"
+  type        = map(string)
+
+  default = {
+    Application = "vigilant-sniffle"
+    Environment = "infra"
+  }
+}
+
 variable "frontend_password" {
   description = "When enabled, this password is required (along with user that's the env name, e.g. 'dev') to access the frontend"
 }

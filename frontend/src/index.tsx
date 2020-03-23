1489c9201ba0bd25b4377b632dae875d64dfa8cd
tommilimmot <tommilimmot@users.noreply.github.com>
2020-03-24 00:36:14 +0200

    Submit form, Data contains only answered values

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,3 +1,50 @@
-import './index.css';
+import $ from 'jquery';
+import { v4 as uuidV4 } from 'uuid';
 
-console.log('symptomradar', { API_ENDPOINT: process.env.REACT_APP_API_ENDPOINT });
+function submitSuccessfully() {
+  $('#submit-success').removeClass('hidden');
+  $('#symptom-questionnaire').addClass('hidden');
+}
+
+function submitFailed() {
+  $('#submit-error').removeClass('hidden');
+}
+
+function init() {
+  const endpoint = process.env.REACT_APP_API_ENDPOINT;
+  if (!endpoint) {
+    console.error('Endpoint url missing');
+    return;
+  }
+
+  $('#symptom-questionnaire').submit(function(event) {
+    event.preventDefault();
+
+    // serialize form data into { name: input.name, value: input.value } format
+    // unanswered fields are not included
+    const answers = $(this).serializeArray();
+
+    // TODO: check required fields
+
+    // add uuid and timestamp to the request data
+    // snake_case is preferred for keys in AWS Athena
+    // see https://docs.aws.amazon.com/athena/latest/ug/tables-databases-columns-names.html
+    const requestData: { [key: string]: string } = {
+      participant_uuid: uuidV4(),
+      response_timestamp: new Date().toISOString(),
+    };
+
+    // populate request data with answers
+    for (const answer of answers) {
+      requestData[answer.name] = answer.value;
+    }
+
+    console.log(requestData);
+
+    $.post(endpoint, requestData)
+      .done(submitSuccessfully)
+      .fail(submitFailed);
+  });
+}
+
+$(document).ready(init);

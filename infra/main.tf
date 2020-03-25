29c3134d77c3b02988a1a0a289a33bb833e9973f
Jarno Rantanen <jarno.rantanen@futurice.com>
2020-03-25 09:37:27 +0200

    Remove password in anticipation for first public release.

--- infra/main.tf
+++ infra/main.tf
@@ -9,11 +9,10 @@ module "env_dev" {
   source    = "./modules/main"
   providers = { aws.us_east_1 = aws.us_east_1 } # this alias is needed because ACM is only available in the "us-east-1" region
 
-  name_prefix       = "${var.name_prefix}-dev"
-  tags              = merge(var.tags, { Environment = "dev" })
-  frontend_password = var.frontend_password
-  frontend_domain   = "dev.oiretutka.fi"
-  backend_domain    = "api.dev.oiretutka.fi"
+  name_prefix     = "${var.name_prefix}-dev"
+  tags            = merge(var.tags, { Environment = "dev" })
+  frontend_domain = "dev.oiretutka.fi"
+  backend_domain  = "api.dev.oiretutka.fi"
 }
 
 # Implements an instance of the app, for a specific env
@@ -21,11 +20,10 @@ module "env_prod" {
   source    = "./modules/main"
   providers = { aws.us_east_1 = aws.us_east_1 } # this alias is needed because ACM is only available in the "us-east-1" region
 
-  name_prefix       = "${var.name_prefix}-prod"
-  tags              = merge(var.tags, { Environment = "prod" })
-  frontend_password = var.frontend_password
-  frontend_domain   = "www.oiretutka.fi"
-  backend_domain    = "api.oiretutka.fi"
+  name_prefix     = "${var.name_prefix}-prod"
+  tags            = merge(var.tags, { Environment = "prod" })
+  frontend_domain = "www.oiretutka.fi"
+  backend_domain  = "api.oiretutka.fi"
 }
 
 # Pass along any output from the instantiated module

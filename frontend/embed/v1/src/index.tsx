ca9035634ea0c55d3dc367ea0494c2ed361a824c
Jarno Rantanen <jarno.rantanen@futurice.com>
2020-03-25 12:28:37 +0200

    Encode form as JSON, and POST to API without jQuery.

--- frontend/embed/v1/src/index.tsx
+++ frontend/embed/v1/src/index.tsx
@@ -75,47 +75,21 @@ function init() {
   $('#symptom-questionnaire').submit(function(event) {
     event.preventDefault();
 
-    // serialize form data into { name: input.name, value: input.value } format
-    // unanswered fields are not included
-    const answers: { name: string; value: string }[] = $(this).serializeArray();
-
-    // TODO: check required fields
-
-    // add uuid and timestamp to the request data
-    // snake_case is preferred for keys in AWS Athena
-    // see https://docs.aws.amazon.com/athena/latest/ug/tables-databases-columns-names.html
-    const requestData: { [key: string]: string | null } = {
-      participant_uuid: getParticipantId(),
-      timestamp: new Date().toISOString(),
-      fever: null,
-      cough: null,
-      breathing_difficulties: null,
-      muscle_pain: null,
-      sore_throat: null,
-      rhinitis: null,
-      general_wellbeing: null,
-      duration: null,
-      longterm_medication: null,
-      smoking: null,
-      corona_suspicion: null,
-      age: null,
-      gender: null,
-      postal_code: null,
-    };
-
-    // populate request data with answers
-    for (const answer of answers) {
-      requestData[answer.name] = answer.value;
-    }
-
-    console.log(requestData);
+    const answers = Array.prototype.slice
+      .call(document.querySelectorAll('#symptom-questionnaire input'))
+      .map((el: HTMLInputElement) => (el.type !== 'radio' || el.checked ? { [el.name]: el.value } : {}))
+      .reduce((memo, next) => ({ ...memo, ...next }), {});
+    const meta = { participant_uuid: getParticipantId(), timestamp: new Date().toISOString() };
+    const submission = { ...meta, ...answers };
 
     // persist participant_uuid – it will be reused if participant answers again
-    storeParticipantId(requestData.participant_uuid as string);
+    storeParticipantId(submission.participant_uuid);
 
-    $.post(endpoint, requestData)
-      .done(submitSuccessfully)
-      .fail(submitFailed);
+    fetch(endpoint, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(submission),
+    }).then(res => (res.ok ? submitSuccessfully() : submitFailed()));
   });
 }
 

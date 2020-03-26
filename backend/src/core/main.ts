fbf6b466219963ae73cb9a6f6ddd725e29ffd2ee
Jarno Rantanen <jarno.rantanen@futurice.com>
2020-03-26 14:52:13 +0200

    Extract S3 key generation logic to its own function.

--- backend/src/core/main.ts
+++ backend/src/core/main.ts
@@ -6,17 +6,23 @@ const bucket = process.env.BUCKET_NAME_STORAGE || '';
 
 if (!bucket) throw new Error('Storage bucket name missing from environment');
 
+// Saves the given response into our storage bucket
 export function storeResponseInS3(response: ResponseModelT) {
   const timestamp = new Date().toISOString();
-  const [date, time] = timestamp.split('T');
   const r = { ...response, timestamp }; // browser clock may be wrong/fraudulent, so overwrite with server time
   return s3
     .putObject({
       Bucket: bucket,
-      Key: `responses/raw/${date}/${time}/${r.participant_uuid}.json`,
+      Key: getStorageKey(r),
       Body: JSON.stringify(r),
       ACL: 'private',
     })
     .promise()
     .then(() => {});
+}
+
+// Produces the key under which this response should be stored in S3
+function getStorageKey(response: ResponseModelT): string {
+  const [date, time] = response.timestamp.split('T');
+  return `responses/raw/${date}/${time}/${response.participant_uuid}.json`;
 }

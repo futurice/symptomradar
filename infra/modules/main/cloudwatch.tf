a2777816af5c8783e9ca89bfd57164c7f1ee6243
Jarno Rantanen <jarno.rantanen@futurice.com>
2020-03-27 08:00:04 +0200

    Add some more Lambda stats to overview dashboard.

--- infra/modules/main/cloudwatch.tf
+++ infra/modules/main/cloudwatch.tf
@@ -30,6 +30,78 @@ resource "aws_cloudwatch_dashboard" "overview" {
                     ]
                 }
             }
+        },
+        {
+            "type": "metric",
+            "x": 6,
+            "y": 6,
+            "width": 6,
+            "height": 6,
+            "properties": {
+                "metrics": [
+                    [ "AWS/Lambda", "Duration", "FunctionName", "${module.backend_api.function_name}", "Resource", "${module.backend_api.function_name}", { "stat": "Minimum", "label": "Min" } ],
+                    [ "...", { "stat": "Average", "label": "Avg" } ],
+                    [ "...", { "stat": "Maximum", "label": "Max" } ]
+                ],
+                "region": "${data.aws_region.current.name}",
+                "title": "API call duration",
+                "period": 300,
+                "view": "timeSeries",
+                "stacked": false
+            }
+        },
+        {
+            "type": "metric",
+            "x": 0,
+            "y": 6,
+            "width": 6,
+            "height": 6,
+            "properties": {
+                "metrics": [
+                    [ "AWS/Lambda", "Errors", "FunctionName", "${module.backend_api.function_name}", "Resource", "${module.backend_api.function_name}", { "id": "errors", "color": "#d13212" } ],
+                    [ ".", "Invocations", ".", ".", ".", ".", { "id": "invocations", "visible": false } ],
+                    [ { "expression": "100 - 100 * errors / MAX([errors, invocations])", "label": "Success rate (%)", "id": "availability", "yAxis": "right", "region": "${data.aws_region.current.name}", "visible": false } ]
+                ],
+                "region": "${data.aws_region.current.name}",
+                "title": "API error count",
+                "yAxis": {
+                    "right": {
+                        "max": 100
+                    }
+                },
+                "period": 300,
+                "view": "timeSeries",
+                "stacked": false,
+                "stat": "Sum"
+            }
+        },
+        {
+            "type": "metric",
+            "x": 18,
+            "y": 6,
+            "width": 6,
+            "height": 6,
+            "properties": {
+                "metrics": [
+                    [ "AWS/Lambda", "Throttles", "FunctionName", "${module.backend_api.function_name}", "Resource", "${module.backend_api.function_name}", { "stat": "Sum" } ]
+                ],
+                "region": "${data.aws_region.current.name}",
+                "title": "API throttles"
+            }
+        },
+        {
+            "type": "metric",
+            "x": 12,
+            "y": 6,
+            "width": 6,
+            "height": 6,
+            "properties": {
+                "metrics": [
+                    [ "AWS/Lambda", "ConcurrentExecutions", "FunctionName", "${module.backend_api.function_name}", "Resource", "${module.backend_api.function_name}", { "stat": "Maximum" } ]
+                ],
+                "region": "${data.aws_region.current.name}",
+                "title": "Concurrent API executions"
+            }
         }
     ]
 }

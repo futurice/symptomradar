20fe9d38a82cb87069fa7374f34cfb0e0b2f114c
Olavi Haapala <olavi.haapala@futurice.com>
2020-03-27 17:25:04 +0200

    Fix wrong text in the test description

--- frontend/main/src/App.test.tsx
+++ frontend/main/src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Sivusto päivittyy pian text', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Sivusto päivittyy pian/i);
   expect(linkElement).toBeInTheDocument();

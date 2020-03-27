9b2250a5ca7a81bdcf031728b3b09f839036b5da
Jarno Rantanen <jarno.rantanen@futurice.com>
2020-03-27 11:41:34 +0200

    Fix TF formatting.

--- infra/logging.tf
+++ infra/logging.tf
@@ -92,6 +92,6 @@ POLICY
 }
 
 resource "aws_iam_role_policy_attachment" "replication" {
-  role       = "${aws_iam_role.replication.name}"
-  policy_arn = "${aws_iam_policy.replication.arn}"
+  role       = aws_iam_role.replication.name
+  policy_arn = aws_iam_policy.replication.arn
 }

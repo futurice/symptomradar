98bc3fbb83d4db668670d4bdb3086dc2bed4193a
Olavi Haapala <olavi.haapala@futurice.com>
2020-03-28 12:44:14 +0200

    Add @reach/router

--- frontend/main/src/App.tsx
+++ frontend/main/src/App.tsx
@@ -1,25 +1,28 @@
 import React from 'react';
+import { Router, RouteComponentProps } from '@reach/router';
 import logo from './oiretutka-logo-gradient.svg';
 import './App.css';
 
-function App() {
-  return (
-    <>
-      <header>
-        <img src={logo} className="logo" alt="Oiretutka. Helsingin Sanomat ja Futurice." />
-        <em className="notice">Sivusto päivittyy pian</em>
-      </header>
-      <main>
-        <p>
-          Lue projektista lisää{' '}
-          <a href="https://www.hs.fi/kotimaa/art-2000006452379.html" target="_blank" rel="noopener noreferrer">
-            Helsingin Sanomien artikkelista
-          </a>
-          .
-        </p>
-      </main>
-    </>
-  );
-}
+const Home = (props: RouteComponentProps) => (
+  <>
+    <header>
+      <img src={logo} className="logo" alt="Oiretutka. Helsingin Sanomat ja Futurice." />
+      <em className="notice">Sivusto päivittyy pian</em>
+    </header>
+    <main>
+      <p>
+        Lue projektista lisää{' '}
+        <a href="https://www.hs.fi/kotimaa/art-2000006452379.html" target="_blank" rel="noopener noreferrer">
+          Helsingin Sanomien artikkelista
+        </a>
+        .
+      </p>
+    </main>
+  </>
+);
 
-export default App;
+export const App = () => (
+  <Router>
+    <Home path="/" />
+  </Router>
+);

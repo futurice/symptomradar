4f5fd7c6e81dc3d9323640cf3b764686186bc098
Dean Clatworthy <dean.clatworthy@gmail.com>
2020-03-28 23:41:18 +0200

    Converted translate.js to typescript

--- frontend/embed/v1/translate.ts
+++ frontend/embed/v1/translate.ts
@@ -6,21 +6,24 @@ const translations = require('./src/translations.json');
 
 const defaultLanguage = 'fi';
 
-const getTemplatePath = lng => (defaultLanguage === lng ? `./public/index.html` : `./public/index.${lng}.html`);
+const getTemplatePath = (lng: String): String =>
+  defaultLanguage === lng ? `./public/index.html` : `./public/index.${lng}.html`;
 
-const createLocalizedTemplate = (html, lng, translations) => {
+const createLocalizedTemplate = (html: string, lng: String, translations: Object): void => {
   const path = getTemplatePath(lng);
   const compiled = _.template(html);
   const compiledHTML = compiled(translations);
 
-  fs.writeFile(path, compiledHTML, err => {
+  fs.writeFile(path, compiledHTML, (err: Error) => {
     if (err) return console.log(err);
     console.log(`Created localized index file > ${path}`);
   });
 };
 
-fs.readFile('./public/index.template.html', (err, data) => {
+fs.readFile('./public/index.template.html', (err: Error, data: Buffer) => {
   if (err) throw err;
 
   Object.keys(translations).map(lng => createLocalizedTemplate(data.toString(), lng, translations[lng]));
 });
+
+export {};

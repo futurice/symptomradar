b03847a51eed46ef2cbb8d51693eb55ba70fb0e8
Jarno Rantanen <jarno.rantanen@futurice.com>
2020-03-29 10:22:49 +0300

    Move templated index.html to be the index.html that gets built by webpack.

--- frontend/embed/v1/translate.ts
+++ frontend/embed/v1/translate.ts
@@ -4,13 +4,10 @@ _.templateSettings.interpolate = /{{([\s\S]+?)}}/g;
 const fs = require('fs');
 const translations = require('./src/translations.json');
 
-const defaultLanguage = 'fi';
-
-const getTemplatePath = (lng: String): String =>
-  defaultLanguage === lng ? `./public/index.html` : `./public/index.${lng}.html`;
+const getOutputPath = (lng: String): String => `./build/index.${lng}.html`;
 
 const createLocalizedTemplate = (html: string, lng: String, translations: Object): void => {
-  const path = getTemplatePath(lng);
+  const path = getOutputPath(lng);
   const compiled = _.template(html);
   const compiledHTML = compiled(translations);
 
@@ -20,7 +17,7 @@ const createLocalizedTemplate = (html: string, lng: String, translations: Object
   });
 };
 
-fs.readFile('./public/index.template.html', (err: Error, data: Buffer) => {
+fs.readFile('./build/index.html', (err: Error, data: Buffer) => {
   if (err) throw err;
 
   Object.keys(translations).map(lng => createLocalizedTemplate(data.toString(), lng, translations[lng]));

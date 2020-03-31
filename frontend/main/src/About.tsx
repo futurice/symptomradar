1111f6e0e9e5d53b67a4e196f40c85dbf7d277f2
Lotta Skogström <lotta.skogstrom@futurice.com>
2020-03-31 16:49:50 +0300

    Edit about page styles

--- frontend/main/src/About.tsx
+++ frontend/main/src/About.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 import { RouteComponentProps } from '@reach/router';
+
+const Container = styled.div`
+  margin: 24px;
+`;
 
 const About = (props: RouteComponentProps) => {
   return (
-    <>
+    <Container>
       <p>About</p>
-    </>
+    </Container>
   );
 };
 

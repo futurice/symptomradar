5f88e00c21531b4f342815859569b47c9eaf220c
Lotta Skogström <lotta.skogstrom@futurice.com>
2020-03-31 15:14:46 +0300

    Add new components

---
+++ frontend/main/src/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { RouteComponentProps } from '@reach/router';
+
+const About = (props: RouteComponentProps) => {
+  return (
+    <>
+      <p>About</p>
+    </>
+  );
+};
+
+export default About;

084f81013653e564980febbbbeba9b3bd4b6302f
Lotta Skogström <lotta.skogstrom@futurice.com>
2020-04-01 12:48:49 +0300

    Add form in an  iframe

--- frontend/main/src/About.tsx
+++ frontend/main/src/About.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 import { RouteComponentProps, Link } from '@reach/router';
+import IframeResizer from 'iframe-resizer-react';
 
 const Container = styled.div`
   margin: 24px;
+`;
+
+const Iframe = styled(IframeResizer)`
+  border: none;
+  min-width: 100%;
+  width: 100px;
 `;
 
 const About = (props: RouteComponentProps) => {
@@ -11,6 +18,20 @@ const About = (props: RouteComponentProps) => {
     <Container>
       <p>About</p>
       <Link to="../tietosuojalauseke">Tietosuoja</Link>
+      {/* Settings used at HS 
+      <Iframe
+        id="autoresize45905"
+        className="autoresize"
+        // allowfullscreen="true"
+        // allowvr="true"
+        src="https://www.oiretutka.fi/embed/v1/"
+        width="100%"
+        height=""
+        scrolling="no"
+        frameBorder="0"
+        style={{ border: 'none', width: '100px', minWidth: '100%' }}
+      ></Iframe> */}
+      <Iframe log src="https://www.oiretutka.fi/embed/v1/" style={{ width: '1px', minWidth: '100%' }} />
     </Container>
   );
 };

a479da0861280cfe232a0fab077f9ef02065d122
Lotta Skogström <lotta.skogstrom@futurice.com>
2020-04-01 15:08:31 +0300

    Remove iframe-resizer-react, use the default library

--- frontend/main/src/About.tsx
+++ frontend/main/src/About.tsx
@@ -1,37 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { RouteComponentProps, Link } from '@reach/router';
-import IframeResizer from 'iframe-resizer-react';
+import * as resizer from 'iframe-resizer';
 
 const Container = styled.div`
   margin: 24px;
 `;
 
-const Iframe = styled(IframeResizer)`
-  border: none;
-  min-width: 100%;
-  width: 100px;
-`;
-
 const About = (props: RouteComponentProps) => {
+  useEffect(() => {
+    resizer.iframeResizer({ log: true }, '#formIframe');
+  });
+
   return (
     <Container>
       <p>About</p>
       <Link to="../tietosuojalauseke">Tietosuoja</Link>
-      {/* Settings used at HS 
-      <Iframe
-        id="autoresize45905"
-        className="autoresize"
-        // allowfullscreen="true"
-        // allowvr="true"
+      <iframe
+        id="formIframe"
         src="https://www.oiretutka.fi/embed/v1/"
-        width="100%"
-        height=""
-        scrolling="no"
-        frameBorder="0"
         style={{ border: 'none', width: '100px', minWidth: '100%' }}
-      ></Iframe> */}
-      <Iframe log src="https://www.oiretutka.fi/embed/v1/" style={{ width: '1px', minWidth: '100%' }} />
+      />
     </Container>
   );
 };

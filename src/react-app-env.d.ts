0bd0e45bd43c8577ee76cb5662afbcd1c65f595a
Jarno Rantanen <jarno.rantanen@futurice.com>
2020-04-02 12:18:48 +0300

    Add some missing TS config

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react-scripts" />
+
+// Allow tsc to build our codebase even without webpack-specific tweaks
+// See https://github.com/microsoft/TypeScript-React-Starter/issues/12
+declare module '*.svg';

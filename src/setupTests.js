bf12127ef239ae6d2ff56963400a25c8a40b7be6
Jarno Rantanen <jarno.rantanen@futurice.com>
2020-04-03 11:30:25 +0300

    Document current hashing behaviour in a test case.

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,7 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+// Ensure a test-friendly mock environment
+process.env.BUCKET_NAME_STORAGE = 'non-existing-bucket-for-test-suite';
+process.env.SECRET_HASHING_PEPPER = 'D4GxgVVh0XQCrVb7FiyCal5ZgnRVkiVf'; // despite the name, this isn't actually secret; see https://github.com/futurice/symptomradar/issues/157

5942dbfeb3aa19b584688f60cca2874d53f9ad5a
Lotta Skogström <lotta.skogstrom@futurice.com>
2020-04-03 09:44:16 +0300

    Remove iframe from about page, add ctexts

--- src/frontend/main/About.tsx
+++ src/frontend/main/About.tsx
@@ -1,43 +1,27 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { RouteComponentProps, Link } from '@reach/router';
-import * as resizer from 'iframe-resizer';
 
 const Container = styled.div`
-  max-width: 768px;
-  margin: 24px auto;
-`;
-
-const AboutContent = styled.div`
-  padding: 24px 24px 0;
-`;
-
-const Iframe = styled.iframe`
-  border: none;
-  width: 100px;
-  min-width: 100%;
+  max-width: 648px;
+  margin: 0 auto;
+  padding: 24px;
 `;
 
 const About = (props: RouteComponentProps) => {
-  useEffect(() => {
-    resizer.iframeResizer({ log: false }, '#formIframe');
-  });
-
   return (
     <Container>
-      <AboutContent>
-        <p>
-          Oiretutka on Helsingin Sanomien ja teknologiayhtiö Futuricen kehittämä hanke, jonka tarkoituksena on kerätä
-          suomalaisilta tietoja heidän kokemistaan oireista. Myös muita suomalaisia medioita osallistuu tiedonkeruuseen.
-          Tiedon avulla on tarkoitus ymmärtää paremmin, miten koronavirus mahdollisesti leviää Suomessa. Hanke tehdään
-          avoimen lähdekoodin periaatteella.
-        </p>
-        <p>
-          Tiedot ovat kerätty kyselylomakkeen avulla. Tällä sivustolla näytämme kyselyn tuloksia.{' '}
-          <Link to="../tietosuojalauseke">Lue lisää tietosuojasta täältä.</Link>
-        </p>
-      </AboutContent>
-      <Iframe id="formIframe" title="Oiretutka-kysely" src="/embed/v1/?variant=plain" />
+      <h1>Oiretutka kerää tietoa koronaviruksesta</h1>
+      <p>
+        Oiretutka on Helsingin Sanomien ja teknologiayhtiö Futuricen kehittämä hanke, jonka tarkoituksena on kerätä
+        suomalaisilta tietoja heidän kokemistaan oireista. Myös muita suomalaisia medioita osallistuu tiedonkeruuseen.
+        Tiedon avulla on tarkoitus ymmärtää paremmin, miten koronavirus mahdollisesti leviää Suomessa. Hanke tehdään
+        avoimen lähdekoodin periaatteella.
+      </p>
+      <p>
+        Tiedot ovat kerätty kyselylomakkeen avulla. Tällä sivustolla näytämme kyselyn tuloksia.{' '}
+        <Link to="../tietosuojalauseke">Lue lisää tietosuojasta täältä.</Link>
+      </p>
     </Container>
   );
 };

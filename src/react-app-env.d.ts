19be714bf2d053e2fdb1d34e961d00c1690ee9e3
Jarno Rantanen <jarno.rantanen@futurice.com>
2020-04-08 16:14:07 +0300

    Fix framing behaviour by excluding the child page -part of iframe-resizer lib from the main site bundle.

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -3,3 +3,7 @@
 // Allow tsc to build our codebase even without webpack-specific tweaks
 // See https://github.com/microsoft/TypeScript-React-Starter/issues/12
 declare module '*.svg';
+
+// We can't import the whole "iframe-resizer" library, so its typings aren't useful to us, either.
+// See the import "iframe-resizer/js/iframeResizer" for details.
+declare module 'iframe-resizer/js/iframeResizer';

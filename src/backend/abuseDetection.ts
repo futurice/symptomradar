ef27c7756cda0c68053126950b694f0c58404a1b
Jarno Rantanen <jarno.rantanen@futurice.com>
2020-04-09 23:44:32 +0300

    Add createDynamoDbClient().

--- src/backend/abuseDetection.ts
+++ src/backend/abuseDetection.ts
@@ -1,3 +1,5 @@
+import * as AWS from 'aws-sdk';
+
 type AbuseScore = {
   seen_ip_24h: number;
   seen_ua_24h: number;
@@ -18,4 +20,60 @@ export function performAbuseDetection(
     seen_ua_24h: 0,
     seen_ff_24h: 0,
   });
+}
+
+// Creates a specialized DynamoDB API wrapper for reading/writing abuse detection related data
+export function createDynamoDbClient(tableName: string, ttlSeconds: number) {
+  var ddb = new AWS.DynamoDB({ apiVersion: '2012-08-10' }); // note: for local development, you may need to: AWS.config.update({ region: 'eu-west-1' });
+  return {
+    // Increments the integer value at given key.
+    // If the key doesn't exist, it's created automatically as having value 0, then incremented normally.
+    // Returns the integer value (after being incremented).
+    incrementKey(key: string): Promise<number> {
+      return ddb
+        .updateItem({
+          TableName: tableName,
+          Key: { ADKey: { S: key } },
+          UpdateExpression: 'SET ADVal = if_not_exists(ADVal, :init) + :inc, ADTtl = :ttl',
+          ExpressionAttributeValues: {
+            ':inc': { N: '1' },
+            ':init': { N: '0' },
+            ':ttl': { N: Math.round((Date.now() + 1000 * ttlSeconds) / 1000) + '' },
+          },
+          ReturnValues: 'UPDATED_NEW',
+        })
+        .promise()
+        .then(res => res?.Attributes?.ADVal)
+        .then(unwrapNumber);
+    },
+
+    // Returns the integer values at given keys.
+    // If some keys don't contain values, they're treated as 0's.
+    getValues(keys: string[]): Promise<number[]> {
+      return ddb
+        .batchGetItem({
+          RequestItems: {
+            [tableName]: {
+              AttributesToGet: ['ADKey', 'ADVal'],
+              Keys: keys.map(key => ({ ADKey: { S: key } })),
+            },
+          },
+        })
+        .promise()
+        .then(res =>
+          (res?.Responses?.[tableName] || []).reduce(
+            (memo, next) => ({ ...memo, [next.ADKey.S || '']: unwrapNumber(next.ADVal) }),
+            {} as { [key: string]: number },
+          ),
+        )
+        .then(res => keys.map(key => res[key] || 0));
+    },
+  };
+}
+
+// See interface AttributeValue in DynamoDB
+function unwrapNumber(attrValue?: { N?: string }): number {
+  const num = parseInt(attrValue?.N || '', 10);
+  if (Number.isFinite(num)) return num;
+  throw new Error(`Unexpected number value from DynamoDB: "${attrValue?.N}"`);
 }

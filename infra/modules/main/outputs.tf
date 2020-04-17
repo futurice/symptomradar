0db99a00307a24c55f6676a7241772f4088685b2
Jarno Rantanen <jarno.rantanen@futurice.com>
2020-04-17 15:33:05 +0300

    Add lambda_env to outputs.

--- infra/modules/main/outputs.tf
+++ infra/modules/main/outputs.tf
@@ -17,3 +17,8 @@ output "storage_bucket_arn" {
 output "storage_result_bucket_arn" {
   value = aws_s3_bucket.storage_results.arn
 }
+
+output "lambda_env" {
+  description = "This is exported to make local development easier; just prefix each with 'export' and place them to your `.envrc` to get an equivalent environment"
+  value       = local.lambda_env
+}

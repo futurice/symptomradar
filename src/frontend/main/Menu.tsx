0fd149eb6211831c92b32250d8c59733c5e406bc
Lotta Skogström <lotta.skogstrom@futurice.com>
2020-04-23 10:46:45 +0300

    Remove focus lock

--- src/frontend/main/Menu.tsx
+++ src/frontend/main/Menu.tsx
@@ -1,58 +1,16 @@
 import React from 'react';
-import { Link, Match } from '@reach/router';
 import styled from 'styled-components';
-import AboutIcon from './assets/AboutIcon';
-import MapIcon from './assets/MapIcon';
-import SurveyIcon from './assets/SurveyIcon';
+import { DialogOverlay, DialogContent } from '@reach/dialog';
+import '@reach/dialog/styles.css';
 import CloseIcon from './assets/CloseIcon';
-import { HEADERHEIGHT } from './constants';
+import Navigation from './Navigation';
 
 type MenuProps = {
   menuOpen: boolean;
   setMenuOpen?: any;
 };
 
-type LinkProps = {
-  to: string;
-  linkText: string;
-  icon: (match: { uri: string; path: string } | null) => void;
-  setMenuOpen: any;
-};
-
-interface activeLinkProps {
-  readonly isActive: boolean;
-}
-
-const menuBreakpoint = '600px';
-
-const MenuContainer = styled.div`
-  width: 100vw;
-  max-width: 600px;
-  margin: 0 auto;
-  position: relative;
-  top: -${HEADERHEIGHT}px;
-
-  @media (min-width: ${menuBreakpoint}) {
-    top: 0;
-  }
-`;
-
-const Overlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 1;
-  width: 100vw;
-  height: 100vh;
-  background-color: ${props => props.theme.black};
-  opacity: 0.6;
-
-  @media (min-width: ${menuBreakpoint}) {
-    display: none;
-  }
-`;
-
-const StyledMenu = styled.nav<MenuProps>`
+const MenuContent = styled(DialogContent)`
   display: flex;
   flex-direction: column;
   background: ${props => props.theme.white};
@@ -60,17 +18,9 @@ const StyledMenu = styled.nav<MenuProps>`
   width: 90vw;
   max-width: 324px;
   position: absolute;
-  top: 0;
   right: 0;
-  z-index: 2;
-  display: ${props => (props.menuOpen ? 'flex' : 'none')};
-
-  @media (min-width: ${menuBreakpoint}) {
-    height: auto;
-    width: 324px;
-    top: -1px;
-    border: 1px solid ${props => props.theme.black};
-  }
+  padding: 0;
+  margin: 0;
 `;
 
 const MenuHeader = styled.div`
@@ -80,58 +30,6 @@ const MenuHeader = styled.div`
   display: flex;
   align-items: center;
   padding-left: 8px;
-
-  @media (min-width: ${menuBreakpoint}) {
-    display: none;
-  }
-`;
-
-const MenuContent = styled.div`
-  padding: 24px 42px;
-`;
-
-const LinkContainer = styled.ul`
-  display: flex;
-  flex-direction: column;
-  list-style-type: none;
-  margin: 0;
-  padding: 0;
-`;
-
-const RouterLink = styled(Link)`
-  padding: 6px 6px 6px 0;
-  text-decoration: none;
-  color: inherit;
-  display: flex;
-  align-items: center;
-  font-size: 21px;
-  padding: 16px 0;
-
-  &:not(:last-child) {
-    margin-right: 10px;
-  }
-
-  svg {
-    flex: 0 0 24px;
-    width: 24px;
-    height: auto;
-    margin-right: 8px;
-  }
-
-  &:active {
-    color: inherit;
-  }
-`;
-
-const LinkText = styled.span<activeLinkProps>`
-  margin-left: 4px;
-  font-weight: bold;
-  color: ${props => (props.isActive ? props.theme.black : props.theme.blue)};
-  text-decoration: ${props => (props.isActive ? 'none' : 'underline')};
-
-  &:hover {
-    text-decoration: underline;
-  }
 `;
 
 const CloseButton = styled.button`
@@ -150,61 +48,19 @@ const CloseButton = styled.button`
   }
 `;
 
-const LinkItem = ({ to, linkText, icon, setMenuOpen }: LinkProps) => {
-  return (
-    <Match path={to}>
-      {({ match }) => (
-        <RouterLink to={to} onClick={() => setMenuOpen(false)}>
-          {icon(match)}
-          <LinkText isActive={match ? true : false}>{linkText}</LinkText>
-        </RouterLink>
-      )}
-    </Match>
-  );
-};
-
 const Menu = ({ menuOpen, setMenuOpen }: MenuProps) => {
-  const isHidden = menuOpen ? true : false;
   return (
-    <MenuContainer>
-      {menuOpen && <Overlay onClick={() => setMenuOpen(false)} />}
-      <StyledMenu menuOpen={menuOpen} id="main-menu" aria-hidden={!isHidden}>
+    <DialogOverlay isOpen={menuOpen} onDismiss={() => setMenuOpen(false)}>
+      <MenuContent aria-label="blee" id="main-menu">
         <MenuHeader>
           <CloseButton type="button" aria-label="Sulje" onClick={() => setMenuOpen(false)}>
             <CloseIcon />
             Sulje
           </CloseButton>
         </MenuHeader>
-        <MenuContent>
-          <LinkContainer>
-            <li>
-              <LinkItem
-                to="/"
-                linkText="Kartta"
-                setMenuOpen={setMenuOpen}
-                icon={match => <MapIcon fillColor={match ? '#000' : '#0047FF'} />}
-              />
-            </li>
-            <li>
-              <LinkItem
-                to="survey"
-                linkText="Kyselylomake"
-                setMenuOpen={setMenuOpen}
-                icon={match => <SurveyIcon fillColor={match ? '#000' : '#0047FF'} />}
-              />
-            </li>
-            <li>
-              <LinkItem
-                to="about"
-                linkText="Info"
-                setMenuOpen={setMenuOpen}
-                icon={match => <AboutIcon fillColor={match ? '#000' : '#0047FF'} />}
-              />
-            </li>
-          </LinkContainer>
-        </MenuContent>
-      </StyledMenu>
-    </MenuContainer>
+        <Navigation setMenuOpen={setMenuOpen}></Navigation>
+      </MenuContent>
+    </DialogOverlay>
   );
 };
 export default Menu;

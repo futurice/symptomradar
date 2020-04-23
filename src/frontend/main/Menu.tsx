e955934946a8b824fd0eb10b047a920bd52bc14d
Lotta Skogström <lotta.skogstrom@futurice.com>
2020-04-23 12:00:33 +0300

    Render different menu component based on media query

--- src/frontend/main/Menu.tsx
+++ src/frontend/main/Menu.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { DialogOverlay, DialogContent } from '@reach/dialog';
 import '@reach/dialog/styles.css';
+import { useMediaQuery } from 'react-responsive';
 import CloseIcon from './assets/CloseIcon';
 import Navigation from './Navigation';
 
@@ -48,7 +49,23 @@ const CloseButton = styled.button`
   }
 `;
 
-const Menu = ({ menuOpen, setMenuOpen }: MenuProps) => {
+const NavigationContainer = styled.div<MenuProps>`
+  display: ${props => (props.menuOpen ? 'block' : 'none')};
+  z-index: 1;
+  position: relative;
+  width: 100%;
+  max-width: 600px;
+  margin: 0 auto;
+
+  nav {
+    position: absolute;
+    right: 0;
+    top: -1px;
+    border: 1px solid ${props => props.theme.black};
+  }
+`;
+
+const MenuSmallScreen = ({ menuOpen, setMenuOpen }: MenuProps) => {
   return (
     <DialogOverlay isOpen={menuOpen} onDismiss={() => setMenuOpen(false)}>
       <MenuContent aria-label="blee" id="main-menu">
@@ -63,4 +80,29 @@ const Menu = ({ menuOpen, setMenuOpen }: MenuProps) => {
     </DialogOverlay>
   );
 };
+
+const MenuBigScreen = ({ menuOpen, setMenuOpen }: MenuProps) => {
+  return (
+    <NavigationContainer menuOpen={menuOpen} id="main-menu">
+      <Navigation setMenuOpen={setMenuOpen}></Navigation>
+    </NavigationContainer>
+  );
+};
+
+const Menu = ({ menuOpen, setMenuOpen }: MenuProps) => {
+  const isBiggerScreen = useMediaQuery({
+    query: '(min-width: 600px)',
+  });
+
+  return (
+    <>
+      {isBiggerScreen ? (
+        <MenuBigScreen menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      ) : (
+        <MenuSmallScreen menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+      )}
+    </>
+  );
+};
+
 export default Menu;

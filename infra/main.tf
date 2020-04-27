22664238a09703c37d387f43a9c03d0abbf6c2fe
Jarno Rantanen <jarno.rantanen@futurice.com>
2020-04-27 18:49:19 +0300

    Add apex redirect.
    Closes #63

--- infra/main.tf
+++ infra/main.tf
@@ -12,6 +12,7 @@ module "env_dev" {
   name_prefix            = "${var.name_prefix}-dev"
   tags                   = merge(var.tags, { Environment = "dev" })
   frontend_domain        = "dev.oiretutka.fi"
+  apex_redirect_domain   = "test-redirect.oiretutka.fi"
   backend_domain         = "api.dev.oiretutka.fi"
   open_data_domain       = "data.dev.oiretutka.fi"
   known_hashing_pepper   = "D4GxgVVh0XQCrVb7FiyCal5ZgnRVkiVf"
@@ -28,9 +29,10 @@ module "env_prod" {
   source    = "git::ssh://git@github.com/futurice/symptomradar.git//infra/modules/main?ref=v2.3"
   providers = { aws.us_east_1 = aws.us_east_1 } # this alias is needed because ACM is only available in the "us-east-1" region
 
-  name_prefix          = "${var.name_prefix}-prod"
-  tags                 = merge(var.tags, { Environment = "prod" })
-  frontend_domain      = "www.oiretutka.fi"
+  name_prefix     = "${var.name_prefix}-prod"
+  tags            = merge(var.tags, { Environment = "prod" })
+  frontend_domain = "www.oiretutka.fi"
+  # TODO: apex_redirect_domain = "oiretutka.fi"
   backend_domain       = "api.oiretutka.fi"
   open_data_domain     = "data.oiretutka.fi"
   known_hashing_pepper = "vu2xkUW9iGUsIOUqjDEfarmSLmoRJnxB"

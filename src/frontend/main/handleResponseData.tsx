8b8cc104142893807c5e063031a5ea568e104d64
Lotta Skogström <lotta.skogstrom@futurice.com>
2020-05-05 20:22:25 +0300

    Refactor handleResponseData to reduce repetition

--- src/frontend/main/handleResponseData.tsx
+++ src/frontend/main/handleResponseData.tsx
@@ -1,39 +1,23 @@
 const handleResponseData = (data: any) => {
+  console.log(data);
+  const handleData = (value: number) => {
+    return value >= 0 ? value.toLocaleString('fi-FI') : 'ei tietoa';
+  };
+
+  const handlePercetageData = (value: number) => {
+    return data.responses > 25 ? ((value * 100) / data.population).toFixed(2).replace('.', ',') : null;
+  };
+
   const responsesTotalValue = data.responses !== -1 ? data.responses.toLocaleString('fi-FI') : '< 25';
-  const percentageOfPopulationValue =
-    responsesTotalValue !== '< 25' ? ((data.responses * 100) / data.population).toFixed(2).replace('.', ',') : null;
-
-  const suspicionTotalValue =
-    data.corona_suspicion_yes !== -1 ? data.corona_suspicion_yes.toLocaleString('fi-FI') : 'ei tietoa';
-  const suspicionPercentageValue =
-    data.corona_suspicion_yes !== -1
-      ? ((data.corona_suspicion_yes * 100) / data.population).toFixed(2).replace('.', ',')
-      : null;
-
-  const coughTotalValue =
-    data.cough_mild + data.cough_intense !== -2
-      ? (data.cough_mild + data.cough_intense).toLocaleString('fi-FI')
-      : 'ei tietoa';
-  const coughPercentageValue =
-    data.cough_mild + data.cough_intense !== -2
-      ? (((data.cough_mild + data.cough_intense) * 100) / data.population).toFixed(2).replace('.', ',')
-      : null;
-
-  const feverTotalValue =
-    data.fever_slight + data.fever_high !== -2
-      ? (data.fever_slight + data.fever_high).toLocaleString('fi-FI')
-      : 'ei tietoa';
-  const feverPercentageValue =
-    data.fever_slight + data.fever_high !== -2
-      ? (((data.fever_slight + data.fever_high) * 100) / data.population).toFixed(2).replace('.', ',')
-      : null;
-
-  const breathingDifficultiesTotalValue =
-    data.breathing_difficulties_yes !== -1 ? data.breathing_difficulties_yes.toLocaleString('fi-FI') : 'ei tietoa';
-  const breathingDifficultiesPercentageValue =
-    data.breathing_difficulties_yes !== -1
-      ? ((data.breathing_difficulties_yes * 100) / data.population).toFixed(2).replace('.', ',')
-      : null;
+  const percentageOfPopulationValue = handlePercetageData(data.responses);
+  const suspicionTotalValue = handleData(data.corona_suspicion_yes);
+  const suspicionPercentageValue = handlePercetageData(data.corona_suspicion_yes);
+  const coughTotalValue = handleData(data.cough_mild + data.cough_intense);
+  const coughPercentageValue = handlePercetageData(data.cough_mild + data.cough_intense);
+  const feverTotalValue = handleData(data.fever_slight + data.fever_high);
+  const feverPercentageValue = handlePercetageData(data.fever_slight + data.fever_high);
+  const breathingDifficultiesTotalValue = handleData(data.breathing_difficulties_yes);
+  const breathingDifficultiesPercentageValue = handlePercetageData(data.breathing_difficulties_yes);
 
   return {
     responsesTotal: responsesTotalValue,

f06f56fb1023885570f611a9ed708bcc3ef55e7e
Lotta Skogström <lotta.skogstrom@futurice.com>
2020-05-05 20:21:52 +0300

    Fix typo

--- src/frontend/main/constants.ts
+++ src/frontend/main/constants.ts
@@ -6,7 +6,7 @@ export const FILTERS = {
   muscle_pain_yes: { label: 'Lihaskipuja', id: 'muscle_pain_yes' },
   headache_yes: { label: 'Päänsärkyä', id: 'headache_yes' },
   sore_throat_yes: { label: 'Kurkkukipua', id: 'sore_throat_yes' },
-  rhinits_yes: { label: 'Nuhaa', id: 'rhinits_yes' },
+  rhinitis_yes: { label: 'Nuhaa', id: 'rhinitis_yes' },
   stomach_issues_yes: { label: 'Vatsaoireita', id: 'stomach_issues_yes' },
   sensory_issues_yes: { label: 'Aistien heikkenemistä', id: 'sensory_issues_yes' },
 };

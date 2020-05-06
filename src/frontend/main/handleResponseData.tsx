2b78bcfbc69212ce20f8a59c6e0cbd643b596bd4
Lotta Skogström <lotta.skogstrom@futurice.com>
2020-05-06 10:35:44 +0300

    Refactor handleResponseData

--- src/frontend/main/handleResponseData.tsx
+++ src/frontend/main/handleResponseData.tsx
@@ -4,32 +4,33 @@ const handleResponseData = (data: any) => {
     return value >= 0 ? value.toLocaleString('fi-FI') : 'ei tietoa';
   };
 
-  const handlePercetageData = (value: number) => {
+  const handlePercentageData = (value: number) => {
     return data.responses > 25 ? ((value * 100) / data.population).toFixed(2).replace('.', ',') : null;
   };
 
-  const responsesTotalValue = data.responses !== -1 ? data.responses.toLocaleString('fi-FI') : '< 25';
-  const percentageOfPopulationValue = handlePercetageData(data.responses);
-  const suspicionTotalValue = handleData(data.corona_suspicion_yes);
-  const suspicionPercentageValue = handlePercetageData(data.corona_suspicion_yes);
-  const coughTotalValue = handleData(data.cough_mild + data.cough_intense);
-  const coughPercentageValue = handlePercetageData(data.cough_mild + data.cough_intense);
-  const feverTotalValue = handleData(data.fever_slight + data.fever_high);
-  const feverPercentageValue = handlePercetageData(data.fever_slight + data.fever_high);
-  const breathingDifficultiesTotalValue = handleData(data.breathing_difficulties_yes);
-  const breathingDifficultiesPercentageValue = handlePercetageData(data.breathing_difficulties_yes);
-
   return {
-    responsesTotal: responsesTotalValue,
-    percentageOfPopulation: percentageOfPopulationValue,
-    suspicionTotal: suspicionTotalValue,
-    suspicionPercentage: suspicionPercentageValue,
-    coughTotal: coughTotalValue,
-    coughPercentage: coughPercentageValue,
-    feverTotal: feverTotalValue,
-    feverPercentage: feverPercentageValue,
-    breathingDifficultiesTotal: breathingDifficultiesTotalValue,
-    breathingDifficultiesPercentage: breathingDifficultiesPercentageValue,
+    responsesTotal: data.responses !== -1 ? data.responses.toLocaleString('fi-FI') : '< 25',
+    percentageOfPopulation: handlePercentageData(data.responses),
+    suspicionTotal: handleData(data.corona_suspicion_yes),
+    suspicionPercentage: handlePercentageData(data.corona_suspicion_yes),
+    coughTotal: handleData(data.cough_mild + data.cough_intense),
+    coughPercentage: handlePercentageData(data.cough_mild + data.cough_intense),
+    feverTotal: handleData(data.fever_slight + data.fever_high),
+    feverPercentage: handlePercentageData(data.fever_slight + data.fever_high),
+    breathingDifficultiesTotal: handleData(data.breathing_difficulties_yes),
+    breathingDifficultiesPercentage: handlePercentageData(data.breathing_difficulties_yes),
+    musclePainTotal: handleData(data.muscle_pain_yes),
+    musclePainPercentage: handlePercentageData(data.muscle_pain_yes),
+    headacheTotal: handleData(data.headache_yes),
+    headachePercentage: handlePercentageData(data.headache_yes),
+    soreThroatTotal: handleData(data.sore_throat_yes),
+    soreThroatPercentage: handlePercentageData(data.sore_throat_yes),
+    rhinitsTotal: handleData(data.rhinitis_yes),
+    rhinitsPercentage: handlePercentageData(data.rhinitis_yes),
+    stomachIssuesTotal: handleData(data.stomach_issues_yes),
+    stomachIssuesPercentage: handlePercentageData(data.stomach_issues_yes),
+    sensoryIssuesTotal: handleData(data.sensory_issues_yes),
+    sensoryIssuesPercentage: handlePercentageData(data.sensory_issues_yes),
   };
 };
 

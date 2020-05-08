99ea219e9656a499ac980fdcf5a9556a1762b677
Alex Seidler <alex.seidler@futurice.com>
2020-05-08 12:13:27 +0300

    Add postal code level data to open data index exporter

--- src/backend/dataExports/openDataIndex.ts
+++ src/backend/dataExports/openDataIndex.ts
@@ -6,6 +6,7 @@ type OpenDataIndex = Record<string, OpenDataModel<never>>;
 const openDataConstantKeys: Array<keyof AppConstants> = [
   'totalResponsesKey',
   'cityLevelGeneralResultsKey',
+  'postalCodeLevelGeneralResultsKey',
   'dailyTotalsKey',
   'lowPopulationPostalCodesKey',
   'populationPerCityKey',
